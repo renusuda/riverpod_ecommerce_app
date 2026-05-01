d9ee9256e14a1b3897d26747bce7d5c239949db6
Ren Usuda <rururu.developer@gmail.com>
2026-05-01 20:23:10 +0900

    feat: 余白テーマを登録

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce_app/src/pages/product_list_page.dart';
+import 'package:ecommerce_app/src/theme/app_spacing.dart';
 import 'package:flutter/material.dart';
 
 class App extends StatelessWidget {
@@ -9,7 +10,10 @@ class App extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Ecommerce App',
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      theme: ThemeData(
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        extensions: const [AppSpacing.standard],
+      ),
       home: const ProductListPage(),
     );
   }
