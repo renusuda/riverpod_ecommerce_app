e7af13989ac5397bcac6fe0c790efc0e65c982e8
Ren Usuda <rururu.developer@gmail.com>
2026-05-07 02:50:52 +0900

    fix: 注文の初期データを空にする

--- lib/src/features/orders/data/remote/fake_orders_remote_data_source.dart
+++ lib/src/features/orders/data/remote/fake_orders_remote_data_source.dart
@@ -2,24 +2,7 @@ import 'package:ecommerce_app/src/features/orders/data/remote/orders_remote_data
 import 'package:ecommerce_app/src/features/orders/domain/order.dart';
 
 class FakeOrdersRemoteDataSource implements OrdersRemoteDataSource {
-  final List<Order> _orders = [
-    Order(
-      id: 'order-1',
-      userId: 'moc.tset@tset',
-      orderDate: DateTime(2026, 5, 6),
-      orderStatus: OrderStatus.confirmed,
-      items: {'3': 1},
-      total: 2800,
-    ),
-    Order(
-      id: 'order-2',
-      userId: 'moc.tset@tset',
-      orderDate: DateTime(2026, 5, 6),
-      orderStatus: OrderStatus.confirmed,
-      items: {'1': 1, '2': 2},
-      total: 6900,
-    ),
-  ];
+  final List<Order> _orders = [];
   @override
   Future<List<Order>> fetchOrders() async {
     return _orders;
